3dc875fed868d62f200e726bcac9c7a67724dc76
Filipe Siciliano <filipesiciliano@gmail.com>
2015-06-23 01:25:53 -0300

    First Commit

--- php2/index.php
+++ php2/index.php
@@ -1,19 +1,5 @@
 <?php
-$key = $_REQUEST['key'];
-$url = "https://assine.gvt.com.br/api/jc?handshake=true";
-
-$opts = array('http' =>
-    array(
-        'method'  => 'POST',
-        'header'  => "Content-Type: text/xml\r\n".
-            "Authorization: Basic ".base64_encode("$key")."\r\n",
-        'timeout' => 60
-    )
-);
-
-$context  = stream_context_create($opts);
-$response = file_get_contents($url, false, $context, -1, 40000);
-
-
-//$response = file_get_contents($url);
-echo $response;
+$url = "http://planosnamedida.com.br/Home/ConsultarDisponibilidade";
+$content = file_get_contents($url);
+$content = str_replace('</head>','<link rel="stylesheet" href="http://www.yourwebsiteurl.com/google.css" /></head>', $content);
+echo $content;
